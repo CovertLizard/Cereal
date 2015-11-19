ef1f742862f9bec0b27ccb9f4ccbb350a69e145b
CovertLizard <CovertLizard@users.noreply.github.com>
2015-11-18 21:25:53 -0500

    Version number update, and fixed constructors

--- src/main/java/com/github/covertlizard/cereal/Config.java
+++ src/main/java/com/github/covertlizard/cereal/Config.java
@@ -7,8 +7,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,12 +31,12 @@ public class Config extends YamlConfiguration
      * @param defaults the default values for the configuration file
      * @param sections the sections to be created
      */
-    public Config(File file, String header, Map<String, Object> defaults, String... sections)
+    public Config(File file, String header, Map<String, Object> defaults, List<String> sections)
     {
         this.file = file;
         this.header = header == null ? "" : header;
         if(!this.file.exists()) this.save();
-        Arrays.asList(sections).forEach(string -> {if(!super.contains(string)) super.createSection(string); this.save();});
+        sections.forEach(string -> {if(!super.contains(string)) super.createSection(string); this.save();});
         this.load();
         super.addDefaults(defaults == null ? new HashMap<String, Object>() : defaults);
         super.options().copyDefaults(defaults != null);
@@ -52,7 +52,7 @@ public class Config extends YamlConfiguration
      * @param defaults the default values for the configuration file
      * @param sections the sections to be created
      */
-    public Config(JavaPlugin plugin, String directory, String name, String header, Map<String, Object> defaults, String... sections)
+    public Config(JavaPlugin plugin, String directory, String name, String header, Map<String, Object> defaults, List<String> sections)
     {
         this(new File(plugin.getDataFolder().getPath() + File.separator + directory, name.endsWith(".yml") ? name : name + ".yml"), header, defaults, sections);
     }
@@ -65,7 +65,7 @@ public class Config extends YamlConfiguration
      * @param header the header of the configuration file
      * @param sections the sections to be created
      */
-    public Config(JavaPlugin plugin, String directory, String name, String header, String... sections)
+    public Config(JavaPlugin plugin, String directory, String name, String header, List<String> sections)
     {
         this(plugin, directory, name, header, null, sections);
     }
@@ -77,7 +77,7 @@ public class Config extends YamlConfiguration
      * @param name the name of the configuration file
      * @param sections the sections to be created
      */
-    public Config(JavaPlugin plugin, String directory, String name, String... sections)
+    public Config(JavaPlugin plugin, String directory, String name, List<String> sections)
     {
         this(plugin, directory, name, null, sections);
     }
